cea4ffcc4d3eafbb4c0d7489771cb8de237808d9
aurmer <aubrey.snider@gmail.com>
2019-06-29 13:18:47 -0500

    tic-tac-toe: tried to ensure I loop over the board only once AND tried to make sure my logic easy to follow with well-named variables and methods

--- exercises/257-tic-tac-toe.js
+++ exercises/257-tic-tac-toe.js
@@ -23,3 +23,59 @@
 //   [null, 'X', 'X']
 //   ])
 // null
+function ticTacToe(gameBoard) {
+    var paths = {
+      row0: 'empty',
+      row1: 'empty',
+      row2: 'empty',
+      col0: 'empty',
+      col1: 'empty',
+      col2: 'empty',
+      diag0: 'empty',
+      diag1: 'empty',
+      updatePath: function(ref,boardElement) {
+        if(this[ref] === "empty") { //if it is still empty, fill with first value found
+          this[ref] = boardElement
+        } else if (this[ref] !== boardElement) { //if it is mismatch, change to false
+          this[ref] = null
+        }
+      },
+      isOnDiag0: function(boardIndexX,boardIndexY) {
+        return (boardIndexX + boardIndexY === 0) || (boardIndexX + boardIndexY === 4) || (boardIndexX === 1 && boardIndexY === 1)
+      },
+      isOnDiag1: function(boardIndexX,boardIndexY) {
+        return (boardIndexX + boardIndexY === 2)
+      },
+      findWinCondition: function(){
+        if(this.row0 !== null) return this.row0
+        if(this.row1 !== null) return this.row1
+        if(this.row2 !== null) return this.row2
+        if(this.col0 !== null) return this.col0
+        if(this.col1 !== null) return this.col1
+        if(this.col2 !== null) return this.col2
+        if(this.diag0 !== null) return this.diag0
+        if(this.diag1 !== null) return this.diag1
+        return null
+      }
+    }
+
+    gameBoard.forEach( (element,index) => {
+      element.forEach( (subElement,subIndex) => {
+
+        //row update
+        paths.updatePath(`row${index}`, subElement)
+        //column check
+        paths.updatePath(`col${subIndex}`, subElement)
+
+        //diaganal check
+        if(paths.isOnDiag0(subIndex,index)){
+          paths.updatePath(`diag0`, subElement)
+        }
+        if(paths.isOnDiag1(subIndex,index)){
+          paths.updatePath(`diag1`, subElement)
+        }
+    })
+  })
+
+  return paths.findWinCondition()
+}
