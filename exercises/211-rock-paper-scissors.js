40d7a5910d7f76e8c27bee999a8c272860579cd4
aurmer <aubrey.snider@gmail.com>
2019-07-02 18:59:38 -0500

    refactored paper-scissors-rock

--- exercises/211-rock-paper-scissors.js
+++ exercises/211-rock-paper-scissors.js
@@ -6,21 +6,37 @@
 // rockPaperScissors('rock', 'scissors') --> 'player 1'
 // rockPaperScissors('rock', 'paper') --> 'player 2'
 // rockPaperScissors('paper', 'paper') --> 'draw'
-function rockPaperScissors(p1,p2) {
-    switch(p1) {
-        case "rock":
-            if(p2 == "rock") return "draw"
-            else if(p2 == "paper") return "player 2"
-            else if(p2 == "scissors") return "player 1"
-        case "paper":
-            if(p2 == "rock") return "player 1"
-            else if(p2 == "paper") return "draw"
-            else if(p2 == "scissors") return "player 2"
-        case "scissors":
-            if(p2 == "rock") return "player 2"
-            else if(p2 == "paper") return "player 1"
-            else if(p2 == "scissors") return "draw"
-        default:
-        return "draw"
-    }
+var gamesLookup = {
+  rockrock: "draw",
+  scissorsscissors: "draw",
+  paperpaper: "draw",
+  rockscissors: "player 1",
+  rockpaper: "player 2",
+  scissorsrock: "player 2",
+  scissorspaper: "player 1",
+  paperscissors: "player 2",
+  paperrock: "player 1"
 }
+
+function rockPaperScissors(throw1,throw2) {
+  return gamesLookup[throw1 + throw2]
+}
+
+// function rockPaperScissors(p1,p2) {
+//     switch(p1) {
+//         case "rock":
+//             if(p2 == "rock") return "draw"
+//             else if(p2 == "paper") return "player 2"
+//             else if(p2 == "scissors") return "player 1"
+//         case "paper":
+//             if(p2 == "rock") return "player 1"
+//             else if(p2 == "paper") return "draw"
+//             else if(p2 == "scissors") return "player 2"
+//         case "scissors":
+//             if(p2 == "rock") return "player 2"
+//             else if(p2 == "paper") return "player 1"
+//             else if(p2 == "scissors") return "draw"
+//         default:
+//         return "draw"
+//     }
+// }
